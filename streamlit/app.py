4679068a554cc3306b9d051f81afb1676b4714c5
valeriialeskina <valeriialeskina@users.noreply.github.com>
2021-06-28 23:28:33 +0200

    functions for creating df and modelling

--- streamlit/app.py
+++ streamlit/app.py
@@ -11,7 +11,12 @@ import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer as sia
 nltk.download("vader_lexicon")
 from gensim.parsing.preprocessing import STOPWORDS, strip_tags, strip_numeric, strip_punctuation, strip_multiple_whitespaces, remove_stopwords, strip_short, stem_text
-
+from ta import add_all_ta_features 
+import requests
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import GridSearchCV
 
 #cache the stock tickers so they don't have to load everytime
 @st.cache
@@ -96,3 +101,100 @@ if show_news == True:
 
 if fin == True:
     st.write(stock.financials)
+
+
+
+# building the data for prediction
+def get_news(company, date_from='2021-06-01', date_to=None):
+    '''
+    returns dataframe with average sentiment of news headline and sentiment of news summary for every date in a given timeframe
+    company: symbol, example ZM
+    date_from: string format yyyy-mm-dd
+    date_to: string format yyyy-mm-dd
+    '''
+    sid = sia()
+    if date_to is None:
+        date_to = datetime.today().strftime("%Y-%m-%d")
+    URL = 'https://finnhub.io/api/v1/company-news?symbol={}&from={}&to={}&token=c2si65iad3ic1qis06lg'.format(company, date_from, date_to)
+    r = requests.get(URL)
+    news_df = pd.DataFrame(r.json())
+    news_df['datetime'] = [datetime.utcfromtimestamp(i).strftime('%Y-%m-%d') for i in news_df.datetime]
+    news_df.drop(['id','image', 'related','source', 'url'], axis=1, inplace=True)
+    news_df['headline_sentiment'] = [sid.polarity_scores(c)['compound'] for c in news_df['headline']]
+    news_df['summary_sentiment'] = [sid.polarity_scores(c)['compound'] for c in news_df['summary']]
+    
+    news_dates = news_df.groupby(['datetime']).mean().sort_index().reset_index()
+
+    news_dates['Date'] = pd.to_datetime(news_dates['datetime'], format='%Y-%m-%d')
+    return news_dates
+
+
+def get_recommendation_trends(company):
+    URL = 'https://finnhub.io/api/v1/stock/recommendation?symbol={}&token=c2si65iad3ic1qis06lg'.format(company)
+    r = requests.get(URL)
+    recommendation_df = pd.DataFrame(r.json())
+    dfs = []
+    for i, month in recommendation_df.period.iteritems():
+        period = month
+        df_month = pd.DataFrame({
+        'period': pd.date_range(
+            start = pd.Timestamp(period),                        
+            end = pd.Timestamp(period) + pd.offsets.MonthEnd(0),  # <-- 2018-08-31 with MonthEnd
+            freq = 'D'
+            )
+        })
+        df_month['month'] = month
+        dfs.append(df_month)
+    df = pd.concat(dfs)
+    df.period = [datetime.strftime(x, '%Y-%m-%d') for x in df.period]
+    df = df.merge(recommendation_df,how='left', left_on='month', right_on='period')
+    df.drop(['month','period_y'], axis=1, inplace=True)
+    df['Date'] = pd.to_datetime(df['period_x'], format='%Y-%m-%d')
+    return df.drop(['period_x'],1)
+
+add_all_ta_features(
+    hist, open="Open", high="High", low="Low", close="Close", volume="Volume", fillna=True)
+
+news_df = get_news(slct)
+trend_df = get_recommendation_trends(slct)
+data = hist.merge(news_df, how='inner', on='Date').merge(trend_df, how='inner', on='Date').drop(['symbol','datetime_y','datetime_x'],1)
+st.write('Data to train the model on')
+st.write(data)
+
+def predict(data, move_days=7):
+    # shifted values column, like this the model can learn what the price is going to be x days later
+    data["shift_close"]=data[["Close"]].shift(-move_days)
+    #x without the NaNs 
+    X = data.drop(['shift_close','Date'],1)[:-move_days]
+    y = data[['shift_close']][:-move_days]
+    X_train, X_test, y_train, y_test=train_test_split(X,y,test_size=0.2)
+
+    # training a model on the dataset where we have all data
+    param_grid = {'min_samples_leaf': [2,3,5,10,20], 'n_estimators': [3,5,10,50,100], 'max_depth':[20,30,40]}
+    rf = RandomForestRegressor(random_state = 42)
+    grid_search = GridSearchCV(estimator = rf, param_grid = param_grid, cv = 3)
+    grid_search.fit(X_train, y_train)
+    params = grid_search.best_params_
+
+    rf=RandomForestRegressor(min_samples_leaf=params['min_samples_leaf'], n_estimators=params['n_estimators'], max_depth = params['max_depth'],random_state=42)
+    rf.fit(X_train, y_train)
+    y_pred_existing=rf.predict(X_test)
+    MSE = mean_squared_error(y_test, y_pred_existing)
+    
+    # Now the X variable is going to be the features for the days where we have no "in x Days price"
+    last_days=data.tail(move_days)
+    X_pred=last_days.drop(['shift_close','Date'],1)
+    
+    #values for the next 7 days 
+    pred=rf.predict(X_pred)
+    #predicted_dates = data['Date']
+    last_days['shift_close'] = pred
+    
+    return last_days, MSE
+predicted, MSE =predict(data)
+
+st.write('Predicted values')
+st.write(predicted[['Date','shift_close']])
+
+
+st.write('MSE:' + str(MSE))
